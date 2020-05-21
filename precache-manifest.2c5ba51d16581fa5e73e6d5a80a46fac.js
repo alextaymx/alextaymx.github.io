696374a11c1647df76293824b53a393d0083c1a8
Alex Tay <alex@timeteccloud.com>
2020-05-21 15:12:13 +0800

    Updates

---
+++ precache-manifest.2c5ba51d16581fa5e73e6d5a80a46fac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6237f551e175d5323e2b1db3a52db5",
+    "url": "/alextaymx.github.io/index.html"
+  },
+  {
+    "revision": "46d5467338e2e31bfbd1",
+    "url": "/alextaymx.github.io/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "4ce9b02d2a647a866efb",
+    "url": "/alextaymx.github.io/static/js/2.b81e2f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alextaymx.github.io/static/js/2.b81e2f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d5467338e2e31bfbd1",
+    "url": "/alextaymx.github.io/static/js/main.6acde643.chunk.js"
+  },
+  {
+    "revision": "3c4ea63920659281a32a",
+    "url": "/alextaymx.github.io/static/js/runtime-main.619a35ca.js"
+  },
+  {
+    "revision": "8a396670f9169263baeb071880a740f3",
+    "url": "/alextaymx.github.io/static/media/profile.8a396670.jpg"
+  }
+]);
